6ccd3d1375f792ed41f84cb2e81c94d093abdc51
Igor Lee <srleehbb@gmail.com>
2025-05-19 19:54:52 -0300

    Logo component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import { Heading } from './components/Heading';
+import { Container } from './components/Container';
+import { Logo } from './components/Logo';
 
 import './styles/theme.css';
 import './styles/global.css';
-import { Container } from './components/Container';
-// import { TimerIcon } from 'lucide-react';
 
 function App() {
   return (
     <>
       <Container>
-        <Heading>Logo</Heading>
+        <Logo />
       </Container>
       <Container>
         <Heading>Menu</Heading>
