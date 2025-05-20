5949f97a808332ddfd5cc3a2a22662efd8b7665e
Igor Lee <srleehbb@gmail.com>
2025-05-20 18:46:39 -0300

    Countdown component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Menu } from './components/Menu';
 import { Container } from './components/Container';
 import { Logo } from './components/Logo';
+import { Countdown } from './components/Countdown';
 
 import './styles/theme.css';
 import './styles/global.css';
@@ -11,8 +12,13 @@ function App() {
       <Container>
         <Logo />
       </Container>
+
       <Container>
         <Menu />
+      </Container>
+
+      <Container>
+        <Countdown />
       </Container>
     </>
   );
